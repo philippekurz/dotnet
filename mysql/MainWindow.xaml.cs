b5fa08b9ab89548d73d14d0549fde8da644b1290
Philippe <philippe.kurz@gmail.com>
2025-01-09 17:21:25 +0100

    Message de comit

--- mysql/MainWindow.xaml.cs
+++ mysql/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
-using System.Printing;
+using System.Data;
 using System.Windows;
 
 namespace mysql
@@ -15,20 +15,31 @@ namespace mysql
 
         private void Connect()
         {
-            //MessageBox.Show("Connection à la base de données");
-            string connexionString = "server=localhost;database=test;uid=root;password=;";
+            // Local
+            //string connexionString = "server=localhost;database=test;uid=root;password=;";
+            // Distant
+            string connexionString = "server=109.234.165.136;database=judi6669_test;uid=judi6669_philippe;password=e5P[Ey#l^izP;";
+
             MySqlConnection connexion = new MySqlConnection(connexionString);
+            MySqlCommand cmd = new MySqlCommand("SELECT id as Id, email as EMail, name as Nom FROM users;", connexion);
+            DataTable dt = new DataTable();
 
             try {
                 connexion.Open();
-                MessageBox.Show("Base de données ouverte !");
+                //Execute le lecteur de l'objet cmd (lit les données) puis charge les données dans dt avec Load
+                dt.Load(cmd.ExecuteReader());
                 connexion.Close();
             }
             catch(Exception e) {
                 throw new Exception("Erreur à l'ouverture de la base de données : " + e.Message);
             }
 
+            dg.ItemsSource = dt.DefaultView;
+        }
 
+        private void dg_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
+        {
+            MessageBox.Show("end editing");
         }
     }
 }
