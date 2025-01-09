25a251f4320ea867dab856292e6e8493cf7f3a24
Philippe <philippe.kurz@gmail.com>
2025-01-09 11:39:51 +0100

    Changements des params de la fanêtre

--- window/MainWindow.xaml.cs
+++ window/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            // Initialise les composante de la fenêtre
             InitializeComponent();
+
         }
+
     }
 }
