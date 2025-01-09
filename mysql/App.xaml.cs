837644e2bc046f35725b1a275eb041e3c28d05e0
Philippe <philippe.kurz@gmail.com>
2025-01-09 14:36:56 +0100

    Début MySQL

---
+++ mysql/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace mysql
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ mysql/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System.Printing;
+using System.Windows;
+
+namespace mysql
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void Connect()
+        {
+            //MessageBox.Show("Connection à la base de données");
+            string connexionString = "server=localhost;database=test;uid=root;password=;";
+            MySqlConnection connexion = new MySqlConnection(connexionString);
+
+            try {
+                connexion.Open();
+                MessageBox.Show("Base de données ouverte !");
+                connexion.Close();
+            }
+            catch(Exception e) {
+                throw new Exception("Erreur à l'ouverture de la base de données : " + e.Message);
+            }
+
+
+        }
+    }
+}
